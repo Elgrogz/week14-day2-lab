178eeb9ffa5bc75132167e77d9ddd509361ab5ca
Elgrogz <gregorgilchrist@gmail.com>
2017-02-14 13:20:45 +0000

    can select pokemon

--- client/src/components/PokeSelector.jsx
+++ client/src/components/PokeSelector.jsx
@@ -3,13 +3,18 @@ var React = require('react');
 var PokeSelector = React.createClass({
   getInitialState: function() {
     return {selectedIndex: undefined}
-  },  
+  },
+  handleChange: function(event) {
+    var newIndex = event.target.value;
+    this.setState({selectedIndex: newIndex});
+    this.props.selectPokemon(this.props.pokemon[newIndex]);
+  }, 
   render: function() {
     var options = this.props.pokemon.map(function (pokemon, index) {
       return <option value={index} key={index}>{pokemon.name}</option>
     })
     return (
-      <select id="pokemon" value={this.state.selectedIndex}>
+      <select id="pokemon" value={this.state.selectedIndex} onChange={this.handleChange}>
         {options}
       </select>)
   }
