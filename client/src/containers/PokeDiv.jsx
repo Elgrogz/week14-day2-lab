41c5484d4eb2e82b8ae4594eb121878e43d3e369
Elgrogz <gregorgilchrist@gmail.com>
2017-02-14 11:43:38 +0000

    started pokediv, fell in love with jeff

---
+++ client/src/containers/PokeDiv.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var PokeSelector = require('../components/PokeSelector.jsx');
+var Pokedex = require('../components/Pokedex.jsx');
+
+var PokeDiv = React.createClass({
+
+
+
+
+render: function() {
+  return (
+    <div className="poke-div">
+      <h2>Pokemonz</h2>
+      
+    </div>
+    )
+}
+
+
+});
+
+module.exports = PokeDiv;
